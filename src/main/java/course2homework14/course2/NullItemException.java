db2d0a03755c1776077087543fa4b74a33578676
Diana <dianamingazova162@gmail.com>
2023-12-04 18:31:21 +0500

    Начальный  коммит

---
+++ src/main/java/course2homework14/course2/NullItemException.java
@@ -0,0 +1,23 @@
+package course2homework14.course2;
+
+public class NullItemException extends RuntimeException {
+    public NullItemException() {
+    }
+
+    public NullItemException(String message) {
+        super(message);
+    }
+
+    public NullItemException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NullItemException(Throwable cause) {
+        super(cause);
+    }
+
+    public NullItemException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
+
